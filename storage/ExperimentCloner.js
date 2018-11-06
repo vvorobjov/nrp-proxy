vvorobjov/nrp-proxy

commit 0eec86c8c901d07aab7a3df8935a391f925d9913

[NRRPLT-6985] make sure destination exist if assetPath is defined

storage/ExperimentCloner.js
@@ -315,17 +315,17 @@ class ExperimentCloner {
             this.expModelsPaths.robotPath === undefined ||
             this.expModelsPaths.robotPath.custom === false)
         ) {
+          const bodyModelFile = model.__text || model;
           if (model._assetPath == undefined) {
-            const bodyModelFile = model.__text || model;
-            var robotid = model._robotId ? model._robotId : 'robot';
             model = {
               __text: bodyModelFile,
               _assetPath: path.dirname(bodyModelFile),
               _customAsset: false,
               __prefix: bibiConf.__prefix
             };
-            var destFile = path.join(robotid, path.basename(bodyModelFile));
           }
+          var robotid = model._robotId ? model._robotId : 'robot';
+          var destFile = path.join(robotid, path.basename(bodyModelFile));
           this.downloadFile(model.__text, this.config.modelsPath, destFile);
         }
       });

commit ab7e9ecb889e3c99b653ed319032c5b62eb30b00

[NRRPLT-0000] fixed regression introduced in commit 0eec86c for [NRRPLT-6985]

storage/ExperimentCloner.js
@@ -320,6 +320,7 @@ class ExperimentCloner {
             model = {
               __text: bodyModelFile,
               _assetPath: path.dirname(bodyModelFile),
+              _robotId: model._robotId ? model._robotId : 'robot',
               _customAsset: false,
               __prefix: bibiConf.__prefix
             };